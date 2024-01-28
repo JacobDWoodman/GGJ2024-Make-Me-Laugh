4a0f919199c0f2b2e25ac8f79ee2e903c5d5f514
Jacob Woodman <43638991+JacobDWoodman@users.noreply.github.com>
2024-01-28 12:18:09 +0000

    update faces and update some variables for jokes

--- Face.gd
+++ Face.gd
@@ -4,8 +4,7 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-onready var expressions = [load("res://animations/face/face1.png"), load("res://animations/face/face2.png"), load("res://animations/face/face3.png"),]
+export(Array, Image) var expressions
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
